ae6a92e0b272b0f9cda84d4e9b785246b2d9bf15
Meng Yang <671840078@qq.com>
2015-09-15 17:12:58 -0700

    add basic calculator

--- src/leetcode/BasicCalculator.java
+++ src/leetcode/BasicCalculator.java
@@ -1,25 +1,49 @@
 package leetcode;
-import java.util.Scanner;
+
+import java.util.Stack;
 
 public class BasicCalculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "(1- 2)";
-		calculate(str);
+		String str = "(1+(4+5+2)-3)+(6+8)";
+		System.out.println(calculate(str));
 	}
 
 	public static int calculate(String s) {
-		s = s.replaceAll("[\\(\\) ]", "");
-		s = s.replaceAll("[+]", " +");
-		s = s.replaceAll("[-]", " -");
-		int sum = 0;
-		System.out.println(s);
-		Scanner cin = new Scanner(s);
-		while (cin.hasNext()) {
-			sum += cin.nextInt();
+		Stack<Integer> resultStack = new Stack<Integer>();
+		Stack<Integer> signStack = new Stack<Integer>();
+		int result = 0;
+		int sign = 1;
+		int number = 0;
+		for (char c : s.toCharArray()) {
+			if (c >= '0' && c <= '9') {
+				number = number * 10 + (c - '0');
+			} else if (c == '+') {
+				// add the number before the sign
+				result += sign * number;
+				sign = 1;
+				number = 0;
+			} else if (c == '-') {
+				// add the number before the sign
+				result += sign * number;
+				sign = -1;
+				number = 0;
+			} else if (c == '(') {
+				signStack.push(sign);
+				resultStack.push(result);
+				result = 0;
+				sign = 1;
+			} else if (c == ')') {
+				result += sign * number;
+				result *= signStack.pop();
+				result += resultStack.pop();
+				number = 0;
+				sign = 1;
+			}
 		}
-		return sum;
+		if (number != 0)
+			result += sign * number;
+		return result;
 	}
-
 }
