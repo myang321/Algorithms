a8f55c488a75173bdafdb6c559cc190597f2ccc5
Meng Yang <671840078@qq.com>
2015-09-21 17:30:41 -0700

    add intersection of two linked lists

--- src/codility/FrogJump.java
+++ src/codility/FrogJump.java
@@ -4,6 +4,9 @@ public class FrogJump {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		FrogJump t = new FrogJump();
+		int[] data = { 1, 3, 1, 4, 2, 5 };
+		System.out.println(t.solution(data, 7, 3));
 
 	}
 
@@ -18,19 +21,19 @@ public class FrogJump {
 		int maxReachable = D;
 		int curPos = 0;
 		// each position in A will only be visited once
+		// idx for mark array
+		int idx = maxReachable;
 		for (int i = 0; i < A.length; i++) {
 			mark[A[i]] = true;
 			if (A[i] <= maxReachable && A[i] > curPos) {
-				int prevMaxReachable = maxReachable;
 				curPos = A[i];
 				maxReachable = A[i] + D;
-				int j = prevMaxReachable + 1;
 				// each position in mark will only be visited once
-				while (j <= maxReachable && j < X) {
-					if (mark[j]) {
-						maxReachable = j + D;
+				while (idx <= maxReachable && idx < X) {
+					if (mark[idx]) {
+						maxReachable = idx + D;
 					}
-					j++;
+					idx++;
 				}
 			}
 			if (maxReachable >= X)
---
+++ src/microsoft/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,46 @@
+package microsoft;
+
+import leetcode.model.ListNode;
+
+public class IntersectionofTwoLinkedLists {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null)
+			return null;
+		int len1 = listLen(headA);
+		int len2 = listLen(headB);
+		if (len1 > len2)
+			headA = getN(headA, len1 - len2);
+		else
+			headB = getN(headB, len2 - len1);
+		while (headA != null) {
+			if (headA == headB)
+				return headA;
+			headA = headA.next;
+			headB = headB.next;
+		}
+		return null;
+
+	}
+
+	private int listLen(ListNode head) {
+		int cnt = 0;
+		while (head != null) {
+			cnt++;
+			head = head.next;
+		}
+		return cnt;
+	}
+
+	private ListNode getN(ListNode head, int n) {
+		while (n-- > 0) {
+			head = head.next;
+		}
+		return head;
+	}
+}
