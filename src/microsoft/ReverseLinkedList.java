6f1ba03764274c3cc45e187a90e41b23d833969c
Meng Yang <671840078@qq.com>
2015-09-25 23:56:15 -0700

    add word breaker ...

---
+++ src/microsoft/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package microsoft;
+
+import leetcode.model.ListNode;
+
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode reverseList(ListNode head) {
+
+	}
+
+}
---
+++ src/microsoft/WordBreak.java
@@ -0,0 +1,17 @@
+package microsoft;
+
+import java.util.LinkedList;
+import java.util.Set;
+
+public class WordBreak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+	}
+
+	public boolean wordBreak(String s, Set<String> wordDict) {
+
+	}
+
+}
