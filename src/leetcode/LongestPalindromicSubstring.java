dca5bd8c655296ef39671814c120a60440f8aec3
Meng Yang <671840078@qq.com>
2015-10-06 19:40:38 -0700

    add longest palindromic substring

--- src/leetcode/LongestPalindromicSubstring.java
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -1,29 +1,40 @@
 package leetcode;
+
 public class LongestPalindromicSubstring {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "ccc";
-		System.out.println(longestPalindrome(s));
+		LongestPalindromicSubstring l = new LongestPalindromicSubstring();
+		String s = "abcba";
+		System.out.println(l.longestPalindrome(s));
 	}
 
-	public static String longestPalindrome(String s) {
-		char[] c = s.toCharArray();
+
+	public String longestPalindrome(String s) {
+		if (s.isEmpty())
+			return "";
+		if (s.length() == 1)
+			return s;
 		int len = s.length();
-		while (len > 0) {
-			int i, start = 0;
-			while (start + len <= s.length()) {
-				for (i = 0; i < len / 2; i++) {
-					if (c[i + start] != c[len - i - 1 + start])
-						break;
-				}
-				if (i == len / 2) {
-					return s.substring(start, start + len);
-				}
-				start++;
+		int begin, end;
+		int max_len = 1;
+		int begin_idx = 0;
+		for (int i = 0; i < len;) {
+			begin = i;
+			end = i;
+			while (end + 1 < len && s.charAt(end) == s.charAt(end + 1))
+				end++;
+			i = end + 1;
+			while (end + 1 < len && begin - 1 >= 0
+					&& s.charAt(begin - 1) == s.charAt(end + 1)) {
+				begin--;
+				end++;
 			}
-			len--;
+			if (end - begin + 1 > max_len) {
+				max_len = end - begin + 1;
+				begin_idx = begin;
+			}
 		}
-		return s.substring(0, 1);
+		return s.substring(begin_idx, begin_idx + max_len);
 	}
 }
