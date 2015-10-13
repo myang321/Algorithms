deec7407a5424e0e6aee72414c58a9e62a115405
Meng Yang <671840078@qq.com>
2015-10-13 11:50:48 -0700

    add num game

---
+++ src/leetcode/NimGame.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class NimGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean canWinNim(int n) {
+		return n % 4 != 0;
+	}
+
+}
