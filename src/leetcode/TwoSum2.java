6fdccac9547832fce49e131fab13610b61a0dbfc
Meng Yang <671840078@qq.com>
2015-10-23 15:20:23 -0700

    add two sum2

--- src/leetcode/TwoSum2.java
+++ src/leetcode/TwoSum2.java
@@ -1,9 +1,14 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class TwoSum2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TwoSum2 t = new TwoSum2();
+		int[] nums = { 2, 7, 11, 15 };
+		System.out.println(Arrays.toString(t.twoSum(nums, 9)));
 
 	}
 
@@ -21,7 +26,19 @@ public class TwoSum2 {
 	// Output: index1=1, index2=2
 
 	public int[] twoSum(int[] numbers, int target) {
-
+		int lo = 0;
+		int hi = numbers.length - 1;
+		int[] result = new int[2];
+		while (lo < hi) {
+			if (numbers[lo] + numbers[hi] == target) {
+				result[0] = lo + 1;
+				result[1] = hi + 1;
+				break;
+			} else if (numbers[lo] + numbers[hi] < target)
+				lo++;
+			else
+				hi--;
+		}
+		return result;
 	}
-
 }
