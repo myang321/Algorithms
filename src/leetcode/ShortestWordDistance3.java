e2e88e282d01540c0bee744199b84a122be14299
Meng Yang <671840078@qq.com>
2015-10-28 21:29:40 -0700

    add shortest word distance 3

--- src/leetcode/ShortestWordDistance3.java
+++ src/leetcode/ShortestWordDistance3.java
@@ -4,6 +4,9 @@ public class ShortestWordDistance3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ShortestWordDistance3 s = new ShortestWordDistance3();
+		String[] words = { "practice", "makes", "perfect", "coding", "makes" };
+		System.out.println(s.shortestWordDistance(words, "makes", "makes"));
 
 	}
 
@@ -24,7 +27,22 @@ public class ShortestWordDistance3 {
 	//
 	// You may assume word1 and word2 are both in the list.
 	public int shortestWordDistance(String[] words, String word1, String word2) {
+		int idx1 = -1;
+		int idx2 = -1;
+		int min = words.length;
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].equals(word1)) {
+				if (word1.equals(word2)) {
+					idx1 = idx2;
+					idx2 = i;
+				} else
+					idx1 = i;
+			} else if (words[i].equals(word2))
+				idx2 = i;
 
+			if (idx1 != -1 && idx2 != -1)
+				min = Math.min(min, Math.abs(idx1 - idx2));
+		}
+		return min;
 	}
-
 }
