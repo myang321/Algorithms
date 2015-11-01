d78b7b99f2ac583470c238188ecf6fcd50d7e55a
Meng Yang <671840078@qq.com>
2015-10-31 17:50:36 -0700

    add bulls and cows

---
+++ src/leetcode/BullsandCows.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class BullsandCows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BullsandCows b = new BullsandCows();
+		b.getHint("12", "21");
+
+	}
+
+	public String getHint(String secret, String guess) {
+		int bulls = 0;
+		int cows = 0;
+		int len = secret.length();
+		// for 0-9
+		int[] hash = new int[10];
+		for (int i = 0; i < len; i++)
+			hash[secret.charAt(i) - '0']++;
+		for (int i = 0; i < len; i++) {
+			char c = guess.charAt(i);
+			if (secret.charAt(i) == c){
+				bulls++;
+				hash[c - '0']--;
+			}
+		}
+		for (int i = 0; i < len; i++) {
+			char c = guess.charAt(i);
+			if (secret.charAt(i) != c && hash[c - '0'] > 0) {
+				cows++;
+				hash[c - '0']--;
+			}
+		}
+		return bulls + "A" + cows + "B";
+	}
+}
